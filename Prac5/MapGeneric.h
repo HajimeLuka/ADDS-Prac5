cdf41a4e5c0490a0bf390f674e0940473babdb54
ADDS-prac1.git <a1826411@student.adelaide.edu.au>
2022-04-06 02:49:56 +0000

    sadge

---
+++ Prac5/MapGeneric.h
@@ -0,0 +1,23 @@
+#ifndef MAPGENERIC_H
+#define MAPGENERIC_H
+
+#include <iostream>
+#include <stdio.h>
+#include <math.h>
+#include <vector>
+
+using namespace std;
+
+
+class MapGeneric{
+    public:
+    MapGeneric();
+    vector<int>;
+    map(vector<int>);
+
+    private:
+    virtual int f(int) = 0;
+};
+
+
+#endif
